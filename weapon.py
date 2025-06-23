447a13f6ae36ac0ed9acd068c71f99e23b21478c
Eunice <reyes05eunice@gmail.com>
2025-06-24 01:56:30 +0800

    Upgraded weapon damage stats

--- weapon.py
+++ weapon.py
@@ -13,25 +13,25 @@ class Weapon:
         return f"{self.name} (Damage: {self.damage})"
 
 # ------------ object creation ------------
-infectious_bite = Weapon("Infectious Bite", 3) # Ghoul
+infectious_bite = Weapon("Infectious Bite", 5) # Ghoul
 
-tackle_and_claws = Weapon("Tackle and Claws", 2) # Ghoul
+tackle_and_claws = Weapon("Tackle and Claws", 4) # Ghoul
 
-bone_spear = Weapon("Bone Spear", 4) # Ghoul
+bone_spear = Weapon("Bone Spear", 6) # Ghoul
 
-false_life = Weapon("False Life", 5) # Necromancer
+false_life = Weapon("False Life", 7) # Necromancer
 
-circle_of_death = Weapon("Circle of Death", 6) # Necromancer
+circle_of_death = Weapon("Circle of Death", 8) # Necromancer
 
-inflict_wounds = Weapon("Inflict Wounds", 2) # Necromancer
+inflict_wounds = Weapon("Inflict Wounds", 4) # Necromancer
 
-paralysis = Weapon("Paralysis", 6) # Ghoul
+paralysis = Weapon("Paralysis", 8) # Ghoul
 
-necrotic_blast = Weapon("Necrotic Blast", 4) # Necromancer
+necrotic_blast = Weapon("Necrotic Blast", 6) # Necromancer
 
-bleeding_scream = Weapon("Bleeding Scream", 5) # Ghoul
+bleeding_scream = Weapon("Bleeding Scream", 7) # Ghoul
 
-ray_of_sickness = Weapon("Ray of Sickness", 3) # Necromancer
+ray_of_sickness = Weapon("Ray of Sickness", 5) # Necromancer
 
 ghoul_weapons = [infectious_bite, tackle_and_claws, bone_spear, bleeding_scream, paralysis]
                 
