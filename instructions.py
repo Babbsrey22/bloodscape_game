2948b8f615bc8706223efca2f045a341abaa3b8e
Eunice <reyes05eunice@gmail.com>
2025-06-23 22:33:29 +0800

    Updated instructions

--- instructions.py
+++ instructions.py
@@ -7,13 +7,13 @@ class Instructions:
         "\t\t⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘\n")
         time.sleep(1.5)
         print("How to play?\n" \
-        "\t\tYou are playing as the MAIN CHARACTER (Lich/Dark Wizard)\n" \
+        "\t\tYou are playing as the Necromancer\n" \
         "\t\tFor each enter, you deal damage to the Ghoul\n" \
         "\t\tYou then have a choice to deal damage again or heal\n" \
         "Good luck, and may the universe have mercy on you")
         time.sleep(3)
         print("\n\t\t⋆༺  𓆩☠︎︎𓆪  ༻⋆\n" \
         "\t\t⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘\n")
-        
+
         break
     
