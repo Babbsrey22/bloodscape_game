fbf16e50bab47d2822c2596be10c6ccbd38630fa
Eunice <reyes05eunice@gmail.com>
2025-06-23 22:03:12 +0800

    Added instructions for game

---
+++ instructions.py
@@ -0,0 +1,19 @@
+import time
+
+class Instructions:
+    while True:
+        print("\n\t\t⋆༺  𓆩☠︎︎𓆪  ༻⋆\n" \
+        "\t\tWelcome to BLOODSCAPE\n" \
+        "\t\t⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘\n")
+        time.sleep(1.5)
+        print("How to play?\n" \
+        "\t\tYou are playing as the MAIN CHARACTER (Lich/Dark Wizard)\n" \
+        "\t\tFor each enter, you deal damage to the Ghoul\n" \
+        "\t\tYou then have a choice to deal damage again or heal\n" \
+        "Good luck, and may the universe have mercy on you")
+        time.sleep(3)
+        print("\n\t\t⋆༺  𓆩☠︎︎𓆪  ༻⋆\n" \
+        "\t\t⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘\n")
+        
+        break
+    
